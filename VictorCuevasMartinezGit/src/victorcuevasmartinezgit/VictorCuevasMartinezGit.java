eaa67c590f218c098086789ec7251fd4844ee893
DAW104 <DAW104@IF01-12.iesmhp.local>
2019-11-27 15:37:45 +0100

    Primer commit realizado 1

--- VictorCuevasMartinezGit/src/victorcuevasmartinezgit/VictorCuevasMartinezGit.java
+++ VictorCuevasMartinezGit/src/victorcuevasmartinezgit/VictorCuevasMartinezGit.java
@@ -15,7 +15,7 @@ public class VictorCuevasMartinezGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola 2");
     }
     
 }
